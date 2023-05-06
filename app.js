ef3cfcfa84f14c0ae1519a7c97de2363585a2e35
GitLabSJ <aeeou12@gmail.com>
2023-05-06 17:22:40 +0900

    feat: 펜 굵기 변경 기능 추가

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
-let isPainting = true;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
 
 function onMove(event) {
   if (isPainting) {
@@ -18,9 +19,16 @@ function startPainting() {
 }
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+function onLineWidthChange(event) {
+  console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
